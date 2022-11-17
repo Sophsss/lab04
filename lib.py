1bc39e1bceabdf06344ee2ad60918abdbc27fbfc
Sofiia Ushenkina <sonyaushen@gmail.com>
2022-11-17 15:29:02 +0500

    list

---
+++ lib.py
@@ -0,0 +1,9 @@
+def lib(a):
+    b = a[0]
+    c = 0
+    for i in range(1, len(a)):
+        for j in range(len(a[i])):
+            if(a[i][j] in b):
+                c += 1
+        b.append(a[i])
+    return c
